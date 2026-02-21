f3a063e027b0f07cd36ce68a067f142672fc5fe8
Nicholas Louque <nicholaslouque@icloud.com>
2026-02-20 21:53:43 -0600

    Enhance simulation launch file by adding launch arguments for robot spawn position

--- src/project_1/launch/simulation.launch.py
+++ src/project_1/launch/simulation.launch.py
@@ -1,23 +1,45 @@
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-
-    # Get package directory
     pkg_sim = get_package_share_directory('project_1')
+    pkg_tb4_gz = get_package_share_directory('turtlebot4_gz_bringup')
 
-    # Path to world file
     world = os.path.join(pkg_sim, 'worlds', 'enviromental.sdf')
 
-    # Launch modern Gazebo (Harmonic / gz)
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    yaw    = LaunchConfiguration('yaw',    default='0.0')
+
+    declare_x   = DeclareLaunchArgument('x_pose', default_value='0.0')
+    declare_y   = DeclareLaunchArgument('y_pose', default_value='0.0')
+    declare_yaw = DeclareLaunchArgument('yaw',    default_value='0.0')
+
     gazebo = ExecuteProcess(
         cmd=['gz', 'sim', world, '-r'],
         output='screen'
     )
 
+    spawn_tb4 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_tb4_gz, 'launch', 'turtlebot4_spawn.launch.py')
+        ),
+        launch_arguments={
+            'x': x_pose,
+            'y': y_pose,
+            'yaw': yaw,
+        }.items(),
+    )
+
     return LaunchDescription([
-        gazebo
+        declare_x,
+        declare_y,
+        declare_yaw,
+        gazebo,
+        spawn_tb4,
     ])
